77f3fbf279b87ed3672489bc0cb7ccea40bba63c
@eduardoLima <eduardoLimacesl@gmail.com>
2023-02-06 11:07:22 -0300

    feat: add congi setwasmpath and pick IFCitem

--- src/app.js
+++ src/app.js
@@ -1,88 +1,35 @@
-import {
-  AmbientLight,
-  AxesHelper,
-  DirectionalLight,
-  GridHelper,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { IFCLoader } from "web-ifc-three/IFCLoader";
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
 
-//Creates the Three.js scene
-const scene = new Scene();
-
-//Object to store the size of the viewport
-const size = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-//Creates the camera (point of view of the user)
-const camera = new PerspectiveCamera(75, size.width / size.height);
-camera.position.z = 15;
-camera.position.y = 13;
-camera.position.x = 8;
-
-//Creates the lights of the scene
-const lightColor = 0xffffff;
-
-const ambientLight = new AmbientLight(lightColor, 0.5);
-scene.add(ambientLight);
-
-const directionalLight = new DirectionalLight(lightColor, 2);
-directionalLight.position.set(0, 10, 0);
-scene.add(directionalLight);
-
-//Sets up the renderer, fetching the canvas of the HTML
-const threeCanvas = document.getElementById("three-canvas");
-const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-//Creates grids and axes in the scene
-const grid = new GridHelper(50, 30);
-scene.add(grid);
-
-const axes = new AxesHelper();
-axes.material.depthTest = false;
-axes.renderOrder = 1;
-scene.add(axes);
-
-//Creates the orbit controls (to navigate the scene)
-const controls = new OrbitControls(camera, threeCanvas);
-controls.enableDamping = true;
-controls.target.set(-2, 0, 0);
-
-//Animation loop
-const animate = () => {
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-};
-
-animate();
-
-//Adjust the viewport to the size of the browser
-window.addEventListener("resize", () => {
-  (size.width = window.innerWidth), (size.height = window.innerHeight);
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(size.width, size.height);
+const container = document.getElementById("viewer-container");
+const viewer = new IfcViewerAPI({
+  container,
+  backgroundColor: new Color(0xffffff),
 });
+viewer.axes.setAxes();
+viewer.grid.setGrid();
 
-// IFC load file feature.
-const ifcLoader = new IFCLoader()
-ifcLoader.ifcManager.setWasmPath("../")
+window.ondblclick = async () => await viewer.IFC.selector.pickIfcItem(true);
+window.onmousemove = async () => await viewer.IFC.selector.prePickIfcItem();
 
-const input = document.getElementById("file-input")
+async function loadIfc(url) {
+  await viewer.IFC.setWasmPath("../");
+  const model = await viewer.IFC.loadIfcUrl(url);
+  await viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+// Button to load IFC file.
+const input = document.getElementById("file-input");
+
 input.addEventListener(
   "change",
+  
   async (changed) => {
-    const ifcURL = URL.createObjectURL(changed.target.files[0]);
-    const model = await ifcLoader.loadAsync(ifcURL);
-    scene.add(model);
+    const file = changed.target.files[0];
+    const ifcURL = URL.createObjectURL(file);
+    loadIfc(ifcURL);
   },
+  
   false
-)
+  );
+
