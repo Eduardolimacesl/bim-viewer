70be070063a1b52d553e62ec7fc026e61682e551
@eduardoLima <eduardoLimacesl@gmail.com>
2023-02-06 14:52:25 -0300

    chore: reorganize files and folders

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: './src/app.js',
+  input: './src/basic/app.js',
   output: [
     {
       format: 'esm',
-      file: './src/bundle.js'
+      file: './src/basic/bundle.js'
     },
   ],
   plugins: [
     resolve(),
+    commonjs()
   ]
 };
--- src/basic/bundle.js
+++ src/basic/bundle.js
@@ -121888,37 +121888,21 @@ class IfcViewerAPI {
     }
 }
 
-const container = document.getElementById("viewer-container");
-const viewer = new IfcViewerAPI({
-  container,
-  backgroundColor: new Color(0xffffff),
-});
-viewer.axes.setAxes();
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+
+// Create grid and axes
 viewer.grid.setGrid();
-
-
-window.ondblclick = async () => await viewer.IFC.selector.pickIfcItem();
-window.onmousemove = async () => await viewer.IFC.selector.prePickIfcItem();
-
+viewer.axes.setAxes();
 
 async function loadIfc(url) {
-  await viewer.IFC.setWasmPath("../");
-  const model = await viewer.IFC.loadIfcUrl(url);
-  await viewer.shadowDropper.renderShadow(model.modelID);
+		await viewer.IFC.setWasmPath("../../node_modules/web-ifc/");	
+  // Load the model
+    const model = await viewer.IFC.loadIfcUrl(url);
+
+		// Add dropped shadow and post-processing efect
+    await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true;
 }
 
-
-// Button to load IFC file.
-const input = document.getElementById("file-input");
-
-input.addEventListener(
-  "change",
-  
-  async (changed) => {
-    const file = changed.target.files[0];
-    const ifcURL = URL.createObjectURL(file);
-    loadIfc(ifcURL);
-  },
-  
-  false
-  );
+loadIfc('../../ifcSamples/modified.ifc');
