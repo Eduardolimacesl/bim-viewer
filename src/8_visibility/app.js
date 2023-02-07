bbea393aca84228f52a1dea51965a44dd7024e50
@eduardoLima <eduardoLimacesl@gmail.com>
2023-02-07 12:54:26 -0300

    chore: add eslint and prittier

--- src/8_visibility/app.js
+++ src/8_visibility/app.js
@@ -1,41 +1,43 @@
-import { IfcViewerAPI } from 'web-ifc-viewer';
-import { Color } from 'three';
+import { IfcViewerAPI } from 'web-ifc-viewer'
+import { Color } from 'three'
 
-main();
+main()
 
 async function main() {
-  const viewer = await setupScene();
-  const ifcModel = await viewer.IFC.loadIfcUrl('../../ifcSamples/TESTED_Simple_project_01.ifc');
-  const allIDs = getAllIds(ifcModel);
-  const subset = getWholeSubset(viewer, ifcModel, allIDs);
-  replaceOriginalModelBySubset(viewer, ifcModel, subset);
-  setupEvents(viewer, allIDs);
+  const viewer = await setupScene()
+  const ifcModel = await viewer.IFC.loadIfcUrl(
+    '../../ifcSamples/TESTED_Simple_project_01.ifc'
+  )
+  const allIDs = getAllIds(ifcModel)
+  const subset = getWholeSubset(viewer, ifcModel, allIDs)
+  replaceOriginalModelBySubset(viewer, ifcModel, subset)
+  setupEvents(viewer, allIDs)
 }
 
 function setupEvents(viewer, allIDs) {
-  window.ondblclick = () => hideClickedItem(viewer);
+  window.ondblclick = () => hideClickedItem(viewer)
   window.onkeydown = (event) => {
     if (event.code === 'Escape') {
-      showAllItems(viewer, allIDs);
+      showAllItems(viewer, allIDs)
     }
-  };
+  }
 }
 
 function getAllIds(ifcModel) {
-  return Array.from(
-    new Set(ifcModel.geometry.attributes.expressID.array),
-    );
+  return Array.from(new Set(ifcModel.geometry.attributes.expressID.array))
 }
 
 function replaceOriginalModelBySubset(viewer, ifcModel, subset) {
-  const items = viewer.context.items;
-  
-  items.pickableIfcModels = items.pickableIfcModels.filter(model => model !== ifcModel);
-  items.ifcModels = items.ifcModels.filter(model => model !== ifcModel);
-  ifcModel.removeFromParent();
-  
-  items.ifcModels.push(subset);
-  items.pickableIfcModels.push(subset);
+  const items = viewer.context.items
+
+  items.pickableIfcModels = items.pickableIfcModels.filter(
+    (model) => model !== ifcModel
+  )
+  items.ifcModels = items.ifcModels.filter((model) => model !== ifcModel)
+  ifcModel.removeFromParent()
+
+  items.ifcModels.push(subset)
+  items.pickableIfcModels.push(subset)
 }
 
 function getWholeSubset(viewer, ifcModel, allIDs) {
@@ -46,16 +48,19 @@ function getWholeSubset(viewer, ifcModel, allIDs) {
     scene: ifcModel.parent,
     removePrevious: true,
     customID: 'full-model-subset',
-  });
+  })
 }
 
 async function setupScene() {
-  const container = document.getElementById('viewer-container');
-  const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
-  viewer.grid.setGrid();
-  viewer.axes.setAxes();
-  await viewer.IFC.setWasmPath('../../node_modules/web-ifc/');
-  return viewer;
+  const container = document.getElementById('viewer-container')
+  const viewer = new IfcViewerAPI({
+    container,
+    backgroundColor: new Color(0xffffff),
+  })
+  viewer.grid.setGrid()
+  viewer.axes.setAxes()
+  await viewer.IFC.setWasmPath('../../node_modules/web-ifc/')
+  return viewer
 }
 
 function showAllItems(viewer, ids) {
@@ -65,17 +70,13 @@ function showAllItems(viewer, ids) {
     removePrevious: false,
     applyBVH: true,
     customID: 'full-model-subset',
-  });
+  })
 }
 
 function hideClickedItem(viewer) {
-  const result = viewer.context.castRayIfc();
-  if (!result) return;
-  const manager = viewer.IFC.loader.ifcManager;
-  const id = manager.getExpressId(result.object.geometry, result.faceIndex);
-  viewer.IFC.loader.ifcManager.removeFromSubset(
-    0,
-    [id],
-    'full-model-subset',
-  );
-}  
+  const result = viewer.context.castRayIfc()
+  if (!result) return
+  const manager = viewer.IFC.loader.ifcManager
+  const id = manager.getExpressId(result.object.geometry, result.faceIndex)
+  viewer.IFC.loader.ifcManager.removeFromSubset(0, [id], 'full-model-subset')
+}
