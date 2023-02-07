ad65d2e038584297335de3f21c380c62702bde20
@eduardoLima <eduardoLimacesl@gmail.com>
2023-02-07 17:07:54 -0300

    feat: study how download properts in json

--- src/14_infor_preprocessing/bundle.js
+++ src/14_infor_preprocessing/bundle.js
@@ -121901,8 +121901,38 @@ init();
 
 async function init() {
   await viewer.IFC.setWasmPath('../../node_modules/web-ifc/');
-  await viewer.IFC.loadIfcUrl('../../ifcSamples/TESTED_Simple_project_01.ifc');
-  await viewer.GLTF.load('../../gltfSamples/police_station.glb');
+  const model = await viewer.IFC.loadIfcUrl(
+    '../../ifcSamples/TESTED_Simple_project_01.ifc'
+  );
+
+  async function exportJson(event) {
+    // Serialize properties
+    const result = await viewer.IFC.properties.serializeAllProperties(
+      model,
+      undefined,
+      (current, total) => {
+        const progress = current / total;
+        const formatted = Math.trunc(progress * 100);
+        console.log(formatted + '%');
+      }
+    );
+
+    // Download the properties as JSON file
+    const file = new File(result, 'properties');
+
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    link.href = URL.createObjectURL(file);
+    link.download = 'properties.json';
+    link.click();
+    link.remove();
+  }
+
+  window.onkeydown = (event) => {
+    if (event.code === 'KeyP') {
+      exportJson();
+    }
+  };
 }
 
 //Select elements and highlight them with any material
