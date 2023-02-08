8875da5219052b702f18682a8c86f7582d60cba0
@eduardoLima <eduardoLimacesl@gmail.com>
2023-02-08 09:46:12 -0300

    feat: add gps-bim-viewer app

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
 export default {
-  input: './src/14_infor_preprocessing/app.js',
+  input: './build/app.js',
   output: [
     {
       format: 'esm',
-      file: './src/14_infor_preprocessing/bundle.js',
+      file: './build/bundle.js',
     },
   ],
   plugins: [resolve(), commonjs()],
